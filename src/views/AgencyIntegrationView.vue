3e992b8c134a1124d083eea59aae16a55e2ffaa9
Christopher Hill <cjameshill@gmail.com>
2022-11-30 17:22:33 +0000

    Formatted Agency Integration

--- src/views/AgencyIntegrationView.vue
+++ src/views/AgencyIntegrationView.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from "vue"; onMounted(() => {
-    function ct(t) {
-        var a =
-            document.head, c = document.createElement("link"); (c.rel = "stylesheet"), (c.href
-                =
-                "https://storage.googleapis.com/crewpass-development-loginbutton/crewpass-loginbutton-js-dev-aug2021.css?v=4"), a.appendChild(c); var
-                    n = document.createElement("script"); (n.type = "text/javascript"), (n.src =
-                        "https://storage.googleapis.com/crewpass-development-loginbutton/crewpass-loginbutton-js-dev-aug2021.js?v=4"), (n.onreadystatechange
-                            = t), (n.onload = t), a.appendChild(n);
-    } ct(function () {
-        new CrewPass({
-            v:
-                "yourseasonalstaff"
-        }).t(function () { })
-    });
-})</script>
+import { onMounted } from "vue";
+onMounted(() => {
+      function ct(t) {var a = document.head,c = document.createElement("link");(c.rel = "stylesheet"),(c.href = "https://storage.googleapis.com/crewpass-production-loginbutton/crewpass-loginbutton-js-prod-aug2021.css?v=4"),a.appendChild(c);var n = document.createElement("script");(n.type = "text/javascript"),(n.src = "https://storage.googleapis.com/crewpass-production-loginbutton/crewpass-loginbutton-js-prod-aug2021.js?v=4"),(n.onreadystatechange = t),(n.onload = t),a.appendChild(n);}
+      ct(function () {new CrewPass({v: "yourseasonalstaff"}).t(function(){})});
+});
+</script>
 
 <template>
     <main class="w-full md:w-4/5 flex flex-col justify-center items-center">
