eae3ab231c5370f411f38bedf350f1ab2b461bba
Christopher Hill <cjameshill@gmail.com>
2023-04-14 12:28:50 +0000

    Added listener to get button setup data

--- src/views/AgencyIntegrationView.vue
+++ src/views/AgencyIntegrationView.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { useEventListener } from "@vueuse/core";
-
-const messages = ref([]);
+import moment from "moment";
+import { useEventListener, useLastChanged, useStorage } from "@vueuse/core";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const messages: any = ref([]);
 const origin = ref("");
+const popupUrl = ref("");
+const commitId = ref("");
+
+const partnerId: any = ref(route.params?.agency || "crewpass");
+const lastChanged = useLastChanged(partnerId);
+const update = () => {
+    window.location.href = `/agency-original-dev/${partnerId.value}`;
+};
 
 useEventListener(window, "message", (message: any) => {
     console.log("message origin: ", message.origin);
     console.log("message: ", message.data);
+    const data =
+        message && message.data && typeof message.data === "string"
+            ? JSON.parse(message.data || "{}")
+            : {};
+    if (message.origin === origin.value && data.target === "crewpass") {
+        popupUrl.value = data.url;
+        commitId.value = data.commitId;
+    }
     if (message.origin !== origin.value) {
         messages.value.push(message);
     }
@@ -19,18 +37,18 @@ onMounted(() => {
             c = document.createElement("link");
         (c.rel = "stylesheet"),
             (c.href =
-                "https://storage.googleapis.com/crewpass-development-loginbutton/crewpass-loginbutton-js-dev-aug2021.css?v=4"),
+                "https://storage.googleapis.com/crewpass-development-loginbutton/crewpass-loginbutton-js-dev-apr2023.css?v=4"),
             a.appendChild(c);
         var n = document.createElement("script");
         (n.type = "text/javascript"),
             (n.src =
-                "https://storage.googleapis.com/crewpass-development-loginbutton/crewpass-loginbutton-js-dev-aug2021.js?v=4"),
+                "https://storage.googleapis.com/crewpass-development-loginbutton/crewpass-loginbutton-js-dev-apr2023.js?v=4"),
             (n.onreadystatechange = t),
             (n.onload = t),
             a.appendChild(n);
     }
     ct(function () {
-        new CrewPass({ v: "yourseasonalstaff" }).t(function () {});
+        new CrewPass({ v: partnerId.value }).t(function () {});
     });
     origin.value = window.location.origin;
 });
@@ -38,7 +56,42 @@ onMounted(() => {
 
 <template>
     <main class="w-full md:w-4/5 flex flex-col justify-center items-center">
-        <h1 id="title" class="text-lg font-medium">Agency Integration - Dev</h1>
+        <h1 id="title" class="text-lg font-medium">
+            Agency Integration Original - Dev
+        </h1>
+        <div
+            class="my-8 flex flex-col justify-center items-center mb-4 overflow-hidden break-all w-2/3"
+        >
+            <label for="agencyId" class="text-sm text-gray-500 py-4"
+                >Partner ID</label
+            >
+            <div class="flex flex-row justify-stretch space-x-2">
+                <input
+                    v-model="partnerId"
+                    type="text"
+                    name="agencyId"
+                    class="text-md font-medium p-4 rounded-3xl border-2 border-gray-200"
+                    placeholder="partnerId"
+                />
+
+                <button
+                    @click="update"
+                    class="py-2 px-4 rounded-3xl text-xl disabled:cursor-not-allowed font-medium"
+                    :class="
+                        lastChanged
+                            ? 'bg-yellow-400 text-gray-700 hover:bg-yellow-300'
+                            : 'bg-gray-200 text-gray-400 hover:bg-gray-200'
+                    "
+                    :disabled="!lastChanged"
+                >
+                    Update
+                </button>
+            </div>
+            <div class="flex flex-col my-4 space-y-4">
+                <span class="text-xs break-all">Popup Url: {{ popupUrl }}</span>
+                <span class="text-xs break-all">Commit ID: {{ commitId }}</span>
+            </div>
+        </div>
         <div id="cp-holder-1" class="w-full">
             <div id="cp-login-wrapper">
                 <div id="cp-login"></div>
@@ -57,7 +110,9 @@ onMounted(() => {
                     <pre class="text-xs text-gray-600">{{
                         message.origin
                     }}</pre>
-                    <pre class="text-xs text-gray-600">{{ message.data }}</pre>
+                    <pre class="text-xs text-gray-600 whitespace-pre-wrap">{{
+                        message.data
+                    }}</pre>
                 </li>
             </ul>
         </div>
