a7094502e4ba9cd16ae0c81cc368077899dca822
Christopher Hill <cjameshill@gmail.com>
2023-04-14 13:01:27 +0000

    Added dev banner

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+const env = import.meta.env.VITE_ENVIRONMENT || "dev";
 </script>
 
 <template>
+    <a
+        v-if="env === 'dev'"
+        href="https://crewpass-demo-web-prod.netlify.app/"
+        target="_blank"
+    >
+        <div
+            class="w-full p-2 m-0 top-0 left-0 bg-red-800 text-white text-center cursor-pointer hover:bg-red-600"
+        >
+            <p class="text-md">
+                This is running in development environment - for live/prod -
+                click here
+            </p>
+        </div>
+    </a>
     <header class="w-full p-8 flex justify-center items-center">
         <div class="wrapper text-center">
             <h1 class="text-xl font-medium mb-6">CrewPass</h1>
